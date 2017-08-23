1d14b7b5507b22e40dd9f03a93ae0e4ea5e22664
otukutun <orehaorz@gmail.com>
2017-08-23 17:55:33 +0900

    Initial Commit

---
+++ CropSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CropSample
+//
+//  Created by otukutun on 8/23/29 H.
+//  Copyright © 29 Heisei otukutun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
