3e4e64893eb8c4d20e76e09baf72be586681d812
otukutun <orehaorz@gmail.com>
2017-08-24 16:24:26 +0900

    Done tutorial.

---
+++ CropSample/CropAreaView.swift
@@ -0,0 +1,14 @@
+//
+//  CropAreaView.swift
+//  CropSample
+//
+//  Created by otukutun on 8/24/29 H.
+//  Copyright © 29 Heisei otukutun. All rights reserved.
+//
+import UIKit
+
+class CropAreaView: UIView {
+    override func point(inside point: CGPoint, with event: UIEvent?) -> Bool {
+        return false
+    }
+}
--- CropSample/ViewController.swift
+++ CropSample/ViewController.swift
@@ -6,10 +6,37 @@
 //  Copyright © 29 Heisei otukutun. All rights reserved.
 //
 
+// REF: https://medium.com/modernnerd-code/how-to-make-a-custom-image-cropper-with-swift-3-c0ec8c9c7884
+
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIScrollViewDelegate {
 
+    @IBOutlet weak var scrollView: UIScrollView! {
+        didSet{
+            scrollView.delegate = self
+            scrollView.minimumZoomScale = 1.0
+            scrollView.maximumZoomScale = 10.0
+        }
+    }
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var cropAreaView: CropAreaView!
+    
+    var cropArea:CGRect {
+        get {
+            let factor = imageView.image!.size.width / view.frame.width
+            let scale = 1 / scrollView.zoomScale
+            let imageFrame = imageView.imageFrame()
+            let x = (scrollView.contentOffset.x + cropAreaView.frame.origin.x - imageFrame.origin.x) * scale * factor
+            let y = (scrollView.contentOffset.y + cropAreaView.frame.origin.y - imageFrame.origin.y) * scale * factor
+            let width = cropAreaView.frame.size.width * scale * factor
+            let height = cropAreaView.frame.size.height * scale * factor
+            return CGRect(x: x, y: y, width: width, height: height)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +48,14 @@ class ViewController: UIViewController {
     }
 
 
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
+    
+    @IBAction func crop(_ sender: Any) {
+        let croppedCGImage = imageView.image?.cgImage?.cropping(to: cropArea)
+        let croppedImage = UIImage(cgImage: croppedCGImage!)
+        imageView.image = croppedImage
+    }
 }
 
